ee297f2304366ab81c449e5b4db7c29dc8923255
sajajula <56951756+sajajula@users.noreply.github.com>
2019-11-02 10:56:02 +0530

    Add files via upload

--- HBMSController.cs
+++ HBMSController.cs
@@ -1,78 +1,119 @@
-﻿using HBMS_WebAPI.Models;
+﻿using HBMS_MVC.Models;
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
-using System.Net;
 using System.Net.Http;
-using System.Web.Http;
+using System.Web;
+using System.Web.Mvc;
 
-namespace HBMS_WebAPI.Controllers
+namespace HBMS_MVC.Controllers
 {
-    public class HBMSController : ApiController
+    public class HBMSController : Controller
     {
         static SqlConnection conn = new SqlConnection("Data Source=NDAMSSQL\\SQLILEARN;Initial Catalog=Training_13Aug19_Pune;User ID=sqluser;Password=sqluser");  //Connection String
         static SqlCommand cmd;     //Will be used to store and execute command
         static SqlDataReader dr;
+        UserAccount LoggedInUser=null;
 
-        
-        [HttpGet]
-        public int UserAlreadyExist(string username,string email,string phoneno)
+        // GET: HBMS
+        public ActionResult Home()
         {
-            int result = 0;
-            try
-            {
-                cmd = new SqlCommand("HBMS.UserAlreadyExist", conn);
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.AddWithValue("@username", username);
-                cmd.Parameters.AddWithValue("@email", email);
-                cmd.Parameters.AddWithValue("@phoneno", phoneno);
-                conn.Open();
-                result = Convert.ToInt32(cmd.ExecuteScalar());
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-            finally
-            {
-                conn.Close();
-            }
+            return View();
+        }
 
-            return result;
+        public ActionResult RegisterUser()
+        {
+            
+            return View();
+        }
+
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        public ActionResult ChangeDetails()
+        {
+            return View();
         }
 
         [HttpPost]
-        public bool PostUser(UserAccount user) //Insert
+        [ValidateAntiForgeryToken]
+        public ActionResult RegisterUser([Bind(Include = "UserId,UserName,Email,PhoneNo,Name,Password,UserType")]UserAccount user,string Confirm_Password)
         {
-            bool registered = false;
-            try
+            if (ModelState.IsValid)
             {
-                cmd = new SqlCommand("HBMS.RegisterUser", conn);
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.AddWithValue("@username", user.UserName);
-                cmd.Parameters.AddWithValue("@email", user.Email);
-                cmd.Parameters.AddWithValue("@phoneno", user.PhoneNo);
-                cmd.Parameters.AddWithValue("@name", user.Name);
-                cmd.Parameters.AddWithValue("@password", user.Password);
-                cmd.Parameters.AddWithValue("@usertype", user.UserType);
-                conn.Open();
-                int result = cmd.ExecuteNonQuery();
-                if (result > 0)
+                int result = 0;
+                using (var client = new HttpClient())
                 {
-                    registered = true;
+                    client.BaseAddress = new Uri("http://localhost:63458/api/");
+                    var responseTask = client.GetAsync($"HBMS/Get?username={user.UserName}&email={user.Email}&phoneno={user.PhoneNo}");
+                    responseTask.Wait();
+
+                    var res = responseTask.Result;
+                    if (res.IsSuccessStatusCode)
+                    {
+                        var readTask = res.Content.ReadAsAsync<Int32>();
+                        readTask.Wait();
+                        result = readTask.Result;
+                    }
                 }
+
+                if (user.Password != Confirm_Password)
+                {
+                    ModelState.AddModelError("Confirm_Password", "Does Not Match With Password");
+                }
+                else if (result == 0)
+                {
+
+                    using (var client = new HttpClient())
+                    {
+                        client.BaseAddress = new Uri("http://localhost:63458/api/");
+                        var postTask = client.PostAsJsonAsync<UserAccount>("HBMS", user);
+                        postTask.Wait();
+
+                        var res = postTask.Result;
+                        if (res.IsSuccessStatusCode)
+                        {
+                            var readTask = res.Content.ReadAsAsync<bool>();
+                            readTask.Wait();
+                            if (readTask.Result)
+                            {
+                                return RedirectToAction("Login", "HBMS");
+                            }
+                        }
+                    }
+                }
+                if (result % 10 == 1)
+                {
+                    ModelState.AddModelError("UserName", "Entered UserName Not Available");
+                }
+                if ((result/10)%10==1)
+                {
+                    ModelState.AddModelError("Email", $"An account already exists with {user.Email}");
+                }
+                if (result / 100 == 1)
+                {
+                    ModelState.AddModelError("PhoneNo", $"An account already exists with {user.PhoneNo}");
+                }
+                
             }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-            finally
-            {
-                conn.Close();
-            }
-            return registered;
+            
+            return View(user);
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult LoggedOut()
+        {
+
+            LoggedInUser = null;
+            return View();
         }
     }
 }
